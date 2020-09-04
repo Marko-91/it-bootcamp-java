95a69582cf81a87b7e83333c670e90f2b2be8e57
Marko <mare_rocknrola@hotmail.com>
2020-09-04 10:48:28 +0200

    homework with grades

---
+++ domaci/domaci_16/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        Ocena[] ocene;
+        Ocena o1 = new Ocena(70, "Istorija", "Februar");
+        Ocena o2 = new Ocena(49, "Istorija", "Februar");
+        Ocena o3 = new Ocena(81, "Istorija", "Februar");
+        Ocena o4 = new Ocena(100, "Istorija", "Februar");
+
+        ocene = new Ocena[4];
+        ocene[0] = o1;
+        ocene[1] = o2;
+        ocene[2] = o3;
+        ocene[3] = o4;
+
+        System.out.println(Ocena.prosek(ocene));
+    }
+}
---
+++ domaci/domaci_16/src/Ocena.java
@@ -0,0 +1,34 @@
+public class Ocena {
+    private int poeni;
+    private String predmet, rok;
+
+    public Ocena(int poeni, String predmet, String rok) {
+        this.poeni = poeni;
+        this.predmet = predmet;
+        this.rok = rok;
+    }
+
+    public int getPoeni() {
+        return this.poeni;
+    }
+
+    public static int oceni(int poeni) {
+        if (poeni <= 50) return 5;
+         else if (poeni >= 51 && poeni < 61) return 6;
+         else if (poeni >= 61 && poeni < 71) return 7;
+         else if (poeni >= 71 && poeni < 81) return 8;
+         else if (poeni >= 81 && poeni < 91) return 9;
+
+        return 10;
+    }
+
+    public static float prosek(Ocena[] ocene) {
+        int l = ocene.length;
+        float sum = 0;
+        for (int i = 0; i < l; i++) {
+            sum += oceni(ocene[i].getPoeni());
+        }
+
+        return Math.round(sum / l);
+    }
+}
